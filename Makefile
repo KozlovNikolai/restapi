8cb0fbbdd242f3aa8b45601634545083102d5c0c
Nikolay Kozlov <gremiha3@gmail.com>
2024-06-09 16:49:38 +0300

    added store, model, tests

--- Makefile
+++ Makefile
@@ -8,3 +8,39 @@ test:
 
 .DEFAULT_GOAL := build
 
+rundb:
+	docker run --name restapi-pg-13.3 -p 25432:5432 -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=dbpass -e POSTGRES_DB=restapi_dev -d postgres:13.3
+
+runtestdb:
+	docker run --name restapi-test-pg-13.3 -p 35432:5432 -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=dbpass -e POSTGRES_DB=restapi_test -d postgres:13.3
+
+
+LOCAL_BIN:=$(CURDIR)/bin
+
+LOCAL_MIGRATION_DIR=./migrations
+LOCAL_MIGRATION_DSN="host=localhost port=25432 dbname=restapi_dev user=dbuser password=dbpass sslmode=disable"
+LOCAL_MIGRATION_DSN_TEST="host=localhost port=35432 dbname=restapi_test user=dbuser password=dbpass sslmode=disable"
+
+install-deps:
+	go install github.com/pressly/goose/v3/cmd/goose@v3.14.0
+
+create-migration:
+	goose -dir ${LOCAL_MIGRATION_DIR} create create_users sql
+
+local-migration-status:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} status -v
+
+local-migration-up:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} up -v
+
+local-migration-down:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} down -v
+
+local-test-migration-status:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN_TEST} status -v
+
+local-test-migration-up:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN_TEST} up -v
+
+local-test-migration-down:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN_TEST} down -v
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/KozlovNikolai/restapi/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -13,6 +14,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New ...
@@ -29,9 +31,14 @@ func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
-	s.logger.Info("starting API server", s.config.BindAddr)
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting API server", s.config.BindAddr)
 
 	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
@@ -42,18 +49,35 @@ func (s *APIServer) configureLogger() error {
 	if err != nil {
 		return err
 	}
+
+	s.logger.SetFormatter(&logrus.TextFormatter{TimestampFormat: "02-01-2006 15:04:05", FullTimestamp: true})
 	s.logger.SetLevel(level)
+
+	s.logger.Info("configure Logger is done, logger level is: ", s.config.LogLevel)
 	return nil
 }
 
 // configureRouter ...
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+	s.logger.Info("configure Router is done.")
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+	s.logger.Info("configure Store is done, store path is: ", s.config.Store.DatabaseURL)
+
+	return nil
 }
 
 // handleHello ...
 func (s *APIServer) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		s.logger.Info("the '/hello' handler has worked")
 		io.WriteString(w, "Hello")
 	}
 }
